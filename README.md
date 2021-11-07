938820c50b3f6199be3472c397f859db4a1666bf
granitosaurus <bernardas.alisauskas@pm.me>
2021-11-07 13:57:48 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tiny Google Finance CLI
+
+Tiny CLI to scrape the latest stock price from finance.google.com
+
+```
+$ pip install --user https://github.com/Granitosaurus/google-finance
+$ google-finance NYSE HPQ
+31.95
+```
---
+++ gfinance.py
@@ -0,0 +1,33 @@
+from xml.dom import NotFoundErr
+import asyncclick as click
+import re
+from aiohttp import ClientSession
+
+
+async def scrape(exchange, symbol, session):
+    """asynchronously scrape current price of EXCHANGE:SYMBOL pair from finance.google.com"""
+    resp = await session.get(
+        f"https://www.google.com/finance/quote/{symbol.upper()}:{exchange.upper()}"
+    )
+    try:
+        return re.search('data-last-price="([\d\.]+)"', await resp.text()).groups()[0]
+    except AttributeError:
+        raise NotFoundErr(f"value for pair {exchange}:{symbol} not found")
+
+
+@click.command()
+@click.argument("exchange")
+@click.argument("symbol")
+async def run(exchange: str, symbol: str) -> str:
+    """Scrape current exchange:symbol pair price"""
+    async with ClientSession() as session:
+        try:
+            print(await scrape(exchange, symbol, session))
+            exit(0)
+        except NotFoundErr:
+            print("nothing found")
+            exit(1)
+
+
+if __name__ == "__main__":
+    run()
